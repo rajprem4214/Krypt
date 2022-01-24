64d9450626e27f4f66fce5f9155f56fb9243931f
rajprem4214 <rajprem4214@gmail.com>
2022-01-24 22:11:19 +0530

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+import Navbar from './components/Navbar';
+import Welcome from './components/Welcome';
+import Services from './components/Services';
+import Transactions from './components/Transactions';
+import Footer from './components/Footer';
+
+function App() {
+  return (
+    <div className="min-h-screen">
+      <div className='gradient-bg-welcome'>
+    <Navbar/>
+    <Welcome />
+
+
+      </div>
+    <Services />
+    <Transactions />
+    <Footer />  
+    </div>
+  );
+}
+
+export default App;
---
+++ src/utils/constants.js
@@ -0,0 +1,5 @@
+import abi from './Transactions.json';
+
+
+export const contractABI = abi.abi;
+export const contractAddress = '0xc4A5bF2676202751fCAE71B98b9e6DdAC9439c6D'
