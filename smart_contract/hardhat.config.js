54ec092471d1a6c97b78c8c15597e4f7bff9f4f2
Prem Raj <85401522+rajprem4214@users.noreply.github.com>
2024-04-25 10:50:40 +0530

    fix: update hardhat configs

--- smart_contract/hardhat.config.js
+++ smart_contract/hardhat.config.js
@@ -6,8 +6,8 @@ module.exports = {
   solidity: '0.8.0',
   networks:{
     kovan: {
-      url: 'https://eth-kovan.alchemyapi.io/v2/DZnOxE2MpUD_0LJZ868jQ155cuyAK2Rp',
-      accounts: [ 'ae61d0df8f98c9b0c86db6a5be67c157d9e561a15eeb75b341b0a7645b35e87f' ]
+      url: '',
+      accounts: [ '' ]
     }
   }
 }
